f2ad17c7982ea2829af2e98de4b68027c28e2d26
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-09 15:24:56 -0500

    Unlisten change event when component is unmount

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -14,10 +14,16 @@ class AuthScreen extends Component {
 
   constructor(props) {
     super(props);
-    Dimensions.addEventListener("change", (dimensions) => {
-      this.setState({
-        viewMode: Dimensions.get('window').height > 500 ? 'portrait' : 'landscape'
-      });
+    Dimensions.addEventListener("change", this.updateStyles);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.updateStyles);
+  }
+
+  updateStyles = (dims) => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
     });
   }
 
