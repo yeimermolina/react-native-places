68947a9ec09a44edfee3b9661f34650d1ea7c260
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-10 17:12:43 -0500

    Add marker

--- src/components/PickLocation/index.js
+++ src/components/PickLocation/index.js
@@ -9,15 +9,41 @@ class PickLocation extends Component {
             longitude: -122.4013726,
             latitudeDelta: 0.0122,
             longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
-        }
+        },
+        locationChosen: false
     }
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            }
+        });
+    }
+
     render() {
+        let marker = null;
+
+        if (this.state.locationChosen) {
+            marker = (<MapView.Marker coordinate={this.state.focusedLocation} />);
+        }
+
         return (
             <View style={styles.container}>
                 <MapView 
                     initialRegion={this.state.focusedLocation}
+                    region={this.state.focusedLocation}
                     style={styles.map}
-                />
+                    onPress={this.pickLocationHandler}
+                >
+                    {marker}
+                </MapView>
                 <View style={styles.button}>
                     <Button title="Locate me" onPress={() => alert("Pick Location!")}/>
                 </View>
