a1171254e6b4b1abcffbf15fd8df6be610920df9
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-10 16:57:42 -0500

    Add React native maps

--- android/app/src/main/java/com/findplaces/MainApplication.java
+++ android/app/src/main/java/com/findplaces/MainApplication.java
@@ -3,6 +3,7 @@ package com.findplaces;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -38,7 +39,8 @@ public class MainApplication extends NavigationApplication {
         // Add additional packages you require here
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
-          new VectorIconsPackage()
+          new VectorIconsPackage(),
+          new MapsPackage()
         );
     }
   
--- src/components/PickLocation/index.js
+++ src/components/PickLocation/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import { View, Button, StyleSheet, Text, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 class PickLocation extends Component {
+    state = {
+        focusedLocation: {
+            latitude: 37.7900352,
+            longitude: -122.4013726,
+            latitudeDelta: 0.0122,
+            longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
-               <View style={styles.placeholder}><Text>Map</Text></View>
+                <MapView 
+                    initialRegion={this.state.focusedLocation}
+                    style={styles.map}
+                />
                 <View style={styles.button}>
                     <Button title="Locate me" onPress={() => alert("Pick Location!")}/>
                 </View>
@@ -19,12 +31,9 @@ const styles = StyleSheet.create({
         width: "100%",
         alignItems: "center"
     },
-    placeholder: {
-        borderWidth: 1,
-        borderColor: "black",
-        backgroundColor: "#eee",
-        width: "80%",
-        height: 150
+    map: {
+        width: "100%",
+        height: 250
     },
     button: {
         margin: 5
