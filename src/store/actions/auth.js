307d1db83e429bdd474f6b5a27caebd5fa1088b5
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-11 15:49:27 -0500

    Add signup action

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,32 @@
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
-    };
+    return dispatch => {
+        console.log("AUTH", authData);
+        dispatch(authSignup(authData));
+    }
 };
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        fetch(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCVeG4SFHzOWO9tOWJddFmFXKQmicBT2yQ`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(err => {
+            console.log("err", err);
+            alert('Something Happened');
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        })
+    };
+}
